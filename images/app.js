6be58e8220fc42116ae4f2ee48e8445de84b61bb
saisumanthkacham <63229560+saisumanthkacham@users.noreply.github.com>
2020-12-07 22:04:20 +0530

    final js script

--- images/app.js
+++ images/app.js
@@ -14,3 +14,19 @@ function errorHandler(error) {
     console.log("error occured", error);
     alert("something went wrong with server! try again after sometime")
 }
+
+function clickHandler() {
+    var inputTxt = txtInput.value; //takin input
+    console.log(inputTxt);
+
+    //callin server for processing
+    fetch(getTranslation(inputTxt))
+        .then(response => response.json())
+        .then(json => {
+            var translatedTxt = json.contents.translated;
+            outputDiv.innerHTML = translatedTxt;
+        })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
